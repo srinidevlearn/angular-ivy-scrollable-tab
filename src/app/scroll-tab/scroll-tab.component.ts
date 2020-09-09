b70c255e7ce82da1b196028fda3d5caafc82200f
srinidevlearn <fordevintend@gmail.com>
2020-09-09 20:05:14 +0530

    3 files modified

--- src/app/scroll-tab/scroll-tab.component.ts
+++ src/app/scroll-tab/scroll-tab.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-scroll-tab',
-  templateUrl: './scroll-tab.component.html',
-  styleUrls: ['./scroll-tab.component.css']
+  selector: "app-scroll-tab",
+  templateUrl: "./scroll-tab.component.html",
+  styleUrls: ["./scroll-tab.component.css"]
 })
 export class ScrollTabComponent implements OnInit {
+  _tabData = Array(15)
+    .fill(0)
+    .map((itm, i) => {
+      return {
+        header: `Tab heading ${i + 1}`
+      };
+    });
+  private _scrollPos = 50;
 
-  _tabData = Array(15).fill(0).map((itm,i)=>{
-    return {
-      header:`Tiranga-DL-21-J-22${i+1}`
-    }
-  });
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  removeData(index: number) {
+    console.log(this._tabData.length);
+    this._tabData.splice(index, 1);
+    console.log(this._tabData.length);
   }
 
-  removeData(index:number){
-     console.log(this._tabData.length);
-     this._tabData.splice(index, 1);
-     console.log(this._tabData.length);
+  scrollRight() {
+    let d = (document.getElementById("scrollContainer").scrollLeft += Math.abs(
+      this._scrollPos
+    ));
+
+    console.log(d);
   }
 
+  scrollLeft() {
+    let d = (document.getElementById("scrollContainer").scrollLeft += -Math.abs(
+      this._scrollPos
+    ));
+    console.log(d);
+  }
 }
